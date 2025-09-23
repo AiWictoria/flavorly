6098c50c6824c4f282502ec91a2738f2d8bf83f0
Viktoria <viktoria.pakhrova@outlook.com>
2025-09-23 17:07:05 +0200

    Added email and password form.groups

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,4 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Form } from "react-bootstrap";
 
 SignIn.route = {
   path: "/signIn",
@@ -11,7 +11,24 @@ export default function SignIn() {
     <>
       <Row>
         <Col>
-          <h2>Sign In</h2>
+          <Form.Group>
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              required
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Enter password"
+              required
+            />
+          </Form.Group>
         </Col>
       </Row>
     </>
