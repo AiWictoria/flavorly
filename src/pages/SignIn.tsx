46834b91818834645cfe705afc00d99c7003fa66
Viktoria <viktoria.pakhrova@outlook.com>
2025-09-23 16:55:02 +0200

    Changed title, activated auto formatting for typescript, started on sign in component

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,15 +1,19 @@
+import { Row, Col } from "react-bootstrap";
+
 SignIn.route = {
   path: "/signIn",
   menuLabel: "Sign In",
-  index: 1
-}
+  index: 1,
+};
 
 export default function SignIn() {
   return (
     <>
-      <h2>Sign In</h2>
-  </>
-)  
+      <Row>
+        <Col>
+          <h2>Sign In</h2>
+        </Col>
+      </Row>
+    </>
+  );
 }
-
-   
