a1ea8fa52c6ffddbc98f6de4857cdfeb0f9b5441
Viktoria <viktoria.pakhrova@outlook.com>
2025-09-25 10:58:36 +0200

    Allowed all userRoles to read recipes in db. Added fetch recipes and mapped them on RecipePage

--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 RecipePage.route = {
   path: "/recipes",
   menuLabel: "Recipes",
@@ -5,9 +6,41 @@ RecipePage.route = {
 };
 
 export default function RecipePage() {
+  const [recipes, setRecipes] = useState<any[]>([]);
+
+  async function fetchRecipes() {
+    const res = await fetch("/api/recipes", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (res.ok) {
+      const data = await res.json();
+      setRecipes(data);
+      console.log(data);
+    } else {
+      alert("Något gick fel");
+    }
+  }
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
   return (
     <>
       <h2>Recipes</h2>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            {recipe.title}
+            <br />
+            Ingredients: {recipe.ingredients}
+            <br />
+            Instructions: {recipe.instructions}
+            <br />
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
