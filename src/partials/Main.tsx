cc03502497b360f2315172775895f7e5d71734d5
Viktoria <viktoria.pakhrova@outlook.com>
2025-09-25 09:27:44 +0200

    Autoformated Main on save with prettier

--- src/partials/Main.tsx
+++ src/partials/Main.tsx
@@ -1,12 +1,12 @@
-
-import { Container } from 'react-bootstrap';
-import SignIn from '../pages/SignIn';
+import { Container } from "react-bootstrap";
+import SignIn from "../pages/SignIn";
 
 export default function Main() {
-
-  return <main className="mt-5">
-    <Container className="mt-5 mb-4">
-      <SignIn/>
-    </Container>
-  </main>;
+  return (
+    <main className="mt-5">
+      <Container className="mt-5 mb-4">
+        <SignIn />
+      </Container>
+    </main>
+  );
 }
