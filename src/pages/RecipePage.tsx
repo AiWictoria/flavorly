aa88193cdb6cdf4a16204441ba8f5bbde3ff10f1
Viktoria <viktoria.pakhrova@outlook.com>
2025-09-28 20:48:16 +0200

    Added recipes into a dropdown for now, implemented useAuth in recipe hook to get user id when creating recipe

--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -1,4 +1,4 @@
-import { Card, Col, Row } from "react-bootstrap";
+import { Card, Col, Dropdown, Row, Form } from "react-bootstrap";
 import { useRecipes } from "../hooks/useRecipes";
 
 RecipePage.route = {
@@ -12,27 +12,36 @@ export default function RecipePage() {
 
   return (
     <>
-      <h2>Recipes</h2>
-      <Row xs="1" md="2" lg="4">
-        {recipes.map((recipe) => (
-          <Col key={recipe.id}>
-            <Card>
-              <Card.Body>
-                <Card.Title>{recipe.title}</Card.Title>
-                <Card.Subtitle>{recipe.category}</Card.Subtitle>
-                <Card.Text>
-                  <strong>Ingredients: </strong>
-                  {recipe.ingredients}
-                </Card.Text>
-                <Card.Text>
-                  <strong>Instructions: </strong>
-                  {recipe.instructions}
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Recipes
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Row xs="1" md="2" lg="4">
+            {recipes.map((recipe) => (
+              <Col key={recipe.id}>
+                <Card>
+                  <Card.Body>
+                    <Card.Title>{recipe.title}</Card.Title>
+                    <Card.Subtitle>{recipe.category}</Card.Subtitle>
+                    <Card.Text>
+                      <strong>Ingredients: </strong>
+                      {recipe.ingredients}
+                    </Card.Text>
+                    <Card.Text>
+                      <strong>Instructions: </strong>
+                      {recipe.instructions}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <Form></Form>
     </>
   );
 }
