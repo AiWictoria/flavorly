4caf1aad6d9e918e9608a2896056f26b9a3aa991
Viktoria <viktoria.pakhrova@outlook.com>
2025-09-29 21:17:20 +0200

    Added styling and state for Modal, next to do login and sign up forms to show in modal

--- src/partials/header/ProfileModal.tsx
+++ src/partials/header/ProfileModal.tsx
@@ -1,4 +1,5 @@
-import { Modal } from "react-bootstrap";
+import { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
 
 interface ProfileModalProps {
   show: boolean;
@@ -6,14 +7,66 @@ interface ProfileModalProps {
 }
 
 export default function ProfileModal({ show, onHide }: ProfileModalProps) {
+  const [view, setView] = useState<"default" | "login" | "signup">("default");
+
+  const handleClose = () => {
+    setView("default");
+    onHide();
+  };
   return (
-    <Modal show={show} onHide={onHide} centered>
-      <Modal.Header closeButton>
-        <Modal.Title>Welcome to Flavorly</Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="text-center">
-        <p>Please log in or sign up to continue</p>
-      </Modal.Body>
-    </Modal>
+    <>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        className="d-flex justify-content-center align-items-center"
+      >
+        <Modal.Header
+          closeButton
+          closeVariant="white"
+          className="bg-primary text-light custom-close"
+        >
+          <Modal.Title className="p-2 p-sm-2">
+            {view === "default" && "Welcome to Flavorly"}
+            {view === "login" && "Sign In"}
+            {view === "signup" && "Create Account"}
+          </Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body className="">
+          {view === "default" && (
+            <>
+              <h5 className="p-2 p-sm-0 m-sm-4">
+                Sign in or create an account to:{" "}
+              </h5>
+              <div className="p-2 mx-sm-4 p-sm-2 fs-6">
+                <p>
+                  <i className="bi bi-heart-fill mx-1 mx-sm-2" /> Save and share
+                  your favorite recipes
+                </p>
+                <p>
+                  <i className="bi bi-basket2-fill mx-1 mx-sm-2" />
+                  Generate shopping list
+                </p>
+                <p>
+                  <i className="bi bi-chat-heart mx-1 mx-sm-2" />
+                  Comment, rate and discuss
+                </p>
+              </div>
+              <div className="d-flex justify-content-center gap-2 gap-sm-5 mb-sm-4">
+                <Button variant="primary" onClick={() => setView("login")}>
+                  Sign In
+                </Button>
+                <Button
+                  variant="outline-primary"
+                  onClick={() => setView("signup")}
+                >
+                  Sign Up
+                </Button>
+              </div>
+            </>
+          )}
+        </Modal.Body>
+      </Modal>
+    </>
   );
 }
