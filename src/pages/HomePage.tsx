bd9bc9e7232bfc43903c2f90e23030efe42f7954
Viktoria <viktoria.pakhrova@outlook.com>
2025-09-29 15:08:08 +0200

    Added justify content and align items center for whole webpage, changed bootstrap variables to match better with home picture

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Row } from "react-bootstrap";
+import { Col, Row, Button } from "react-bootstrap";
 
 HomePage.route = {
   path: "/",
@@ -6,8 +6,18 @@ HomePage.route = {
 export default function HomePage() {
   return (
     <>
-      <h2>Home</h2>
       <div className="home-bg"></div>
+
+      <Row className="text-center">
+        <Col>
+          <h1 className="text-light">Share, Cook, Enjoy</h1>
+          <h3 className="text-light">Find your new favorite recipes</h3>
+          <div className="gap-3">
+            <Button variant="light">Browse Recipes</Button>
+            <Button variant="outline-light">Share a Recipe</Button>
+          </div>
+        </Col>
+      </Row>
     </>
   );
 }
