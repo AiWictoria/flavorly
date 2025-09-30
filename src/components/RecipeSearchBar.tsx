137d9ccb7acc9d2d844de823c059397df277ff58
Viktoria <viktoria.pakhrova@outlook.com>
2025-09-30 23:47:22 +0200

    Added some styling for searchbar

--- src/components/RecipeSearchBar.tsx
+++ src/components/RecipeSearchBar.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Form, Row, Col } from "react-bootstrap";
 
 interface RecipeSearchBarProps {
@@ -5,14 +6,39 @@ interface RecipeSearchBarProps {
 }
 
 export default function RecipeSearchBar({ onSearch }: RecipeSearchBarProps) {
+  const [value, setValue] = useState("");
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const newValue = e.target.value;
+    setValue(newValue);
+    onSearch(newValue);
+  }
+
+  function handleClear() {
+    setValue("");
+    onSearch("");
+  }
+
   return (
     <Row className="mb-4">
       <Col>
-        <Form.Control
-          type="text"
-          placeholder="Search recipes, ingredients, categories..."
-          onChange={(e) => onSearch(e.target.value)}
-        />
+        <div className="position-relative">
+          <Form.Control
+            type="text"
+            placeholder="Search recipes, ingredients, categories..."
+            value={value}
+            onChange={handleChange}
+            className="pe-5 ps-3"
+          />
+          {value && (
+            <i
+              className="bi bi-x-lg position-absolute top-50 end-0 translate-middle-y me-5"
+              role="button"
+              onClick={handleClear}
+            ></i>
+          )}
+          <i className="bi bi-search fs-5 position-absolute top-50 end-0 translate-middle-y me-3 p-0"></i>
+        </div>
       </Col>
     </Row>
   );
